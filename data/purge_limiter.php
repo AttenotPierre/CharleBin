6ddc974b111158b23da578338d16582e73a37da1
AttenotPierre <pierre.attenot7@etu.univ-lorraine.fr>
2026-02-03 17:13:34 +0100

    modif

--- data/purge_limiter.php
+++ data/purge_limiter.php
@@ -1,2 +1,2 @@
 <?php
-$GLOBALS['purge_limiter'] = 1770129404;
+$GLOBALS['purge_limiter'] = 1770135125;
--- data/traffic_limiter.php
+++ data/traffic_limiter.php
@@ -1,4 +1,4 @@
 <?php
 $GLOBALS['traffic_limiter'] = array (
-  'a8ed7e14c244b5f461c31f78ceb996e06a39f760e3a267a66ef1ee6813e4d9f2' => '1770129666',
+  'a8ed7e14c244b5f461c31f78ceb996e06a39f760e3a267a66ef1ee6813e4d9f2' => '1770135125',
 );
